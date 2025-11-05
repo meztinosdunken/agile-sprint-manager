83fa99d478c19bcf3f7ac0cc3da218c7bf736233
meztinosdunken <orlanmorry5462@hotmail.com>
2025-11-05 05:36:42 -0600

    Refactor greeting message in the root route for clarity

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Welcome to Agile Sprint Manager!');
+  res.send('Welcome to the Agile Sprint Manager API!');
 });
 
 io.on('connection', (socket) => {
